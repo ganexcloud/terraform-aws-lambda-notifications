90aecb2fef881cb1cecf0ae634acfc5d2c3650fd
Caio Henrique <caio@ganex.com.br>
2022-05-13 14:34:52 -0300

    docs(examples): Add examples

--- examples/slack/main.tf
+++ examples/slack/main.tf
@@ -2,6 +2,6 @@ module "lambda_notifications" {
   source               = "../../"
   lambda_function_name = "lambda-notifications"
   sns_topic_name       = "lambda-notifications"
-  webhook_url          = "https://discord.com/api/webhooks/xxxxxx/xxxxxxxx"
-  messenger            = "discord"
+  webhook_url          = "https://hooks.slack.com/services/xxxx/xxxxx/xxxxxxxxxxxxxx"
+  messenger            = "slack"
 }
--- examples/squadcast/main.tf
+++ examples/squadcast/main.tf
@@ -1,7 +1,7 @@
-module "lambda_codepipeline_notifications" {
+module "lambda_notifications" {
   source               = "../../"
-  lambda_function_name = "codepipeline-notifications"
-  sns_topic_name       = "codepipeline-notifications"
+  lambda_function_name = "lambda-notifications"
+  sns_topic_name       = "lambda-notifications"
+  webhook_url          = "https://api.squadcast.com/v2/incidents/api/xxxxxxxxxxx"
   messenger            = "squadcast"
-  webhook_url          = "https://api.squadcast.com/v2/incidents/api/xxxxxxxxxxxxxxxxxxxx"
 }
